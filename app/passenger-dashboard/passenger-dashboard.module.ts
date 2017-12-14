d0ba6695f151ac9b2a16ea2314e6502cfd1f95e1
nydi <stefan.nydegger@gmail.com>
2017-12-14 16:47:18 +0100

    test

---
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {HttpModule} from "@angular/http";
+
+// containers
+import {PassengerDashboardComponent} from "./containers/passenger-dashboard/passenger-dashboard.component";
+// components
+import {PassengerDetailComponent} from "./components/passenger-detail/passenger-detail.component";
+import {PassengerCountComponent} from "./components/passenger-count/passenger-count.component";
+// services
+import {PassengerDashboardService} from "./containers/passenger-dashboard/passenger-dashboard.service";
+
+@NgModule({
+  declarations: [PassengerDashboardComponent, PassengerCountComponent, PassengerDetailComponent],
+  imports: [CommonModule, HttpModule],
+  exports: [PassengerDashboardComponent],
+  providers: [PassengerDashboardService]
+})
+
+export class PassengerDashboardModule {
+}
