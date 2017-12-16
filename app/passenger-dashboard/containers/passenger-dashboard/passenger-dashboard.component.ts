ce27e7a3d5aa46ea455397ad5bc0911c3a9e7245
nydi <stefan.nydegger@gmail.com>
2017-12-16 12:05:51 +0100

    another commit

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -32,21 +32,30 @@ export class PassengerDashboardComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('ngOnInit');
-    this.passengers = this.passengerService.getPassengers();
+    this.passengerService
+      .getPassengers()
+      .subscribe((data: Passenger[]) => this.passengers = data);
   }
 
   handleRemove(event: Passenger) {
-    this.passengers = this.passengers.filter((passenger: Passenger) => {
-      return passenger.id !== event.id;
-    })
+    this.passengerService
+      .removePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passengers = this.passengers.filter((passenger: Passenger) => {
+          return passenger.id !== event.id;
+      });
+    });
   }
 
   handleEdit(event: Passenger) {
-    this.passengers = this.passengers.map((passenger: Passenger) => {
-      if(passenger.id === event.id){
-        passenger = Object.assign({}, passenger, event);
-      }
-      return passenger;
-    })
+    this.passengerService.updatePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passengers = this.passengers.map((passenger: Passenger) => {
+          if (passenger.id === event.id) {
+            passenger = Object.assign({}, passenger, event);
+          }
+          return passenger;
+        })
+      });
   }
 }
